6215a5b0ec6ab0ec69c5a222158e5a8f4db12619
Musa Musa <musabrillz@gmail.com>
2020-09-05 08:35:31 +0100

    timer component added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import './App.css';
+import Timer from '../Timer/Timer'
 
-const App = () => <div className="app-container" />;
+const App = () => (
+  <div className="app-container">
+    <Timer />
+  </div>
+);
 
 export default App;
--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import Timer from '../Timer/Timer'
 
 describe('App', () => {
     it('should render a <div />', () => {
         const container = shallow(<App />);
         expect(container.find('div').length).toEqual(1)
+    })
+
+    it('should render the Timer Component', () => {
+      const container = shallow(<App />);
+      expect(container.containsMatchingElement(<Timer />)).toEqual(true);
     })
 })

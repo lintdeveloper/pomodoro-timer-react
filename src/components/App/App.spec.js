53fde8c390daa2c887092788113da053120845da
Musa Musa <musabrillz@gmail.com>
2020-09-05 08:39:15 +0100

    refractored codebase to obey DRY principles

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -4,13 +4,15 @@ import App from './App';
 import Timer from '../Timer/Timer'
 
 describe('App', () => {
-    it('should render a <div />', () => {
-        const container = shallow(<App />);
-        expect(container.find('div').length).toEqual(1)
-    })
+  let container
 
-    it('should render the Timer Component', () => {
-      const container = shallow(<App />);
-      expect(container.containsMatchingElement(<Timer />)).toEqual(true);
-    })
+  beforeEach(() => (container = shallow(<App />)))
+
+  it('should render a <div />', () => {
+    expect(container.find("div").length).toEqual(1)
+  })
+
+  it('should render the Timer Component', () => {
+    expect(container.containsMatchingElement(<Timer />)).toEqual(true);
+  })
 })

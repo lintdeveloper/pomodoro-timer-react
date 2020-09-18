611a3a1c0f4266c5b3ec8410bdbc1dfdf57e51b6
Musa Musa <musabrillz@gmail.com>
2020-09-18 10:03:39 +0100

    add test to show the timer button on the UI

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -3,13 +3,52 @@ import './Timer.css';
 import TimerButton from '../TimerButton/TimerButton';
 
 class Timer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      minutes: 25,
+      seconds: 0,
+      isOn: false
+    }
+  }
+
+  startTimer() {
+    if (this.state.isOn === true) {
+      return;
+    }
+    this.myInterval = setInterval(() => {
+      const { seconds, minutes } = this.state;
+      if (seconds > 0) {
+        this.setState(({ seconds }) => ({
+          seconds: seconds - 1
+        }))
+      }
+      if (seconds === 0) {
+        if ( minutes === 0) {
+          clearInterval(this.myInterval);
+        } else {
+          this.setState(({ minutes }) => ({
+            minutes: minutes - 1,
+            seconds: 59
+          }))
+        }
+      }
+    }, 1000);
+    this.setState({ isOn: true})
+  }
 
   render() {
+    const { minutes, seconds } = this.state;
+    
     return (
       <div className="timer-container">
         <div className="time-display"></div>
         <div className="timer-button-container">
-          <TimerButton/>
+          <TimerButton
+            className="start-timer"
+            buttonAction={this.startTimer}
+            buttonValue={'Start'}
+          />
           <TimerButton/>
           <TimerButton/>
         </div>
--- src/components/Timer/Timer.spec.js
+++ src/components/Timer/Timer.spec.js
@@ -14,8 +14,41 @@ describe('Timer ', () => {
   });
 
   it('should render the instance of the TimerButton component', () => {
-    expect(container.find('TimerButton').length).toBeGreaterThanOrEqual(1);
+    expect(container.find('TimerButton').length).toBeGreaterThanOrEqual(3);
   });
 
+  it('should have state', () => {
+    const timerState = container.state();
+    expect(timerState).not.toBeNull()
+  });
   
-})
+  it('should have the minutes property on state', () => {
+    const timerState = container.state();
+    expect(timerState.minutes).not.toBeUndefined()
+  });
+
+  it('should initialise minutes property to 25', () => {
+    const timerState = container.state();
+    expect(timerState.minutes).toEqual(25)
+  });
+
+  it('should have the seconds property on state', () => {
+    const timerState = container.state();
+    expect(timerState.seconds).not.toBeUndefined()
+  });
+
+  it('should initialise seconds property to 0 on state', () => {
+    const timerState = container.state();
+    expect(timerState.seconds).not.toBeUndefined()
+  });
+
+  it('should have isOn property on state', () => {
+    const timerState = container.state();
+    expect(timerState.isOn).not.toBeUndefined();
+  });
+  
+  it('should initialise isOn property to false on state', () => {
+    const timerState = container.state();
+    expect(timerState.isOn).toBeFalsy();
+  });
+});
